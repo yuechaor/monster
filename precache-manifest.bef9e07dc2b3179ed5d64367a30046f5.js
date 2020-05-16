fc5ec08283d612bfaa81c93f0ba064d4c6ba9ea0
yuechaor <yuechaor@gmail.com>
2020-05-16 23:31:25 +1000

    Updates

---
+++ precache-manifest.bef9e07dc2b3179ed5d64367a30046f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ff2ae3567fc9da8b254c68bcd6ea2a2",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "1bb0e0b0ff5153b9693d",
+    "url": "/monster/static/css/main.b12287a7.chunk.css"
+  },
+  {
+    "revision": "aa86dba7ccdda618ee4d",
+    "url": "/monster/static/js/2.f5912c83.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster/static/js/2.f5912c83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bb0e0b0ff5153b9693d",
+    "url": "/monster/static/js/main.1b14bf83.chunk.js"
+  },
+  {
+    "revision": "c7a13468bf5b949298ca",
+    "url": "/monster/static/js/runtime-main.108968cb.js"
+  }
+]);
